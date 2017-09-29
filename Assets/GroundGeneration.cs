ad2d5da5d6c81404e74bb1e78b0388df1abeae43
TWalk007 <walkertb03@gmail.com>
2017-09-29 13:13:45 -0400

    Initial Commit 2

---
+++ Assets/GroundGeneration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundGeneration : MonoBehaviour
+{
+
+    GameObject ground;
+
+    private void Awake()
+    {
+        ground = transform.gameObject;
+
+
+    }
+
+    List<GameObject> groundList = new List<GameObject>();
+
+}
+
