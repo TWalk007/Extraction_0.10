bdca5ce331d598fbb8f89c487f16f6f3be3ab7d2
Tom Walker <walkertb03@gmail.com>
2017-11-21 16:15:31 -0500

    LOD Optimization

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -1,96 +1,151 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Threading;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class MapGenerator : MonoBehaviour {
 
-    //We don't want to override our noise map, so we'll create an enum.
     public enum DrawMode {NoiseMap, ColorMap, Mesh};
     public DrawMode drawMode;
 
-    public const int mapChunkSize = 241;
+    public const int mapChunkSize = 65;
     [Range(0,6)]
-    public int levelOfDetail;
+    public int editorPreviewLOD;
     public float noiseScale;
-    public float meshHeightMultiplier;
-    public AnimationCurve meshHeightCurve;
-    [Range(5,12)]
+
     public int octaves;
     [Range(0,1)]
-    public float persistence;
+    public float persistance;
     public float lacunarity;
 
     public int seed;
     public Vector2 offset;
 
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
+
     public bool autoUpdate;
 
     public TerrainType[] regions;
 
-    public void GenerateMap()
-    {
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistence, lacunarity, offset);
+    Queue<MapThreadInfo<MapData>> mapDataThreadInfoQueue = new Queue<MapThreadInfo<MapData>>();
+    Queue<MapThreadInfo<MeshData>> meshDataThreadInfoQueue = new Queue<MapThreadInfo<MeshData>>();
+
+    public void DrawMapInEditor() {
+        MapData mapData = GenerateMapData (Vector2.zero);
+
+        MapDisplay display = FindObjectOfType<MapDisplay> ();
+        if (drawMode == DrawMode.NoiseMap) {
+            display.DrawTexture (TextureGenerator.TextureFromHeightMap (mapData.heightMap));
+        } else if (drawMode == DrawMode.ColorMap) {
+            display.DrawTexture (TextureGenerator.TextureFromColorMap (mapData.colorMap, mapChunkSize, mapChunkSize));
+        } else if (drawMode == DrawMode.Mesh) {
+            display.DrawMesh (MeshGenerator.GenerateTerrainMesh (mapData.heightMap, meshHeightMultiplier, meshHeightCurve, editorPreviewLOD), TextureGenerator.TextureFromColorMap (mapData.colorMap, mapChunkSize, mapChunkSize));
+        }
+    }
+
+    public void RequestMapData(Vector2 center, Action<MapData> callback) {
+        ThreadStart threadStart = delegate {
+            MapDataThread (center, callback);
+        };
+        new Thread (threadStart).Start ();
+    }
+
+    void MapDataThread(Vector2 center, Action<MapData> callback) {
+        MapData mapData = GenerateMapData (center);
+        lock (mapDataThreadInfoQueue) {
+            mapDataThreadInfoQueue.Enqueue (new MapThreadInfo<MapData> (callback, mapData));
+        }
+    }
+
+    public void RequestMeshData(MapData mapData, int lod, Action<MeshData> callback) {
+        ThreadStart threadStart = delegate {
+            MeshDataThread (mapData, lod, callback);
+        };
+        new Thread (threadStart).Start ();
+    }
+
+    void MeshDataThread(MapData mapData, int lod, Action<MeshData> callback) {
+        MeshData meshData = MeshGenerator.GenerateTerrainMesh (mapData.heightMap, meshHeightMultiplier, meshHeightCurve, lod);
+        lock (meshDataThreadInfoQueue) {
+            meshDataThreadInfoQueue.Enqueue (new MapThreadInfo<MeshData> (callback, meshData));
+        }
+    }
+
+    void Update() {
+        if (mapDataThreadInfoQueue.Count > 0) {
+            for (int i = 0; i < mapDataThreadInfoQueue.Count; i++) {
+                MapThreadInfo<MapData> threadInfo = mapDataThreadInfoQueue.Dequeue ();
+                threadInfo.callback (threadInfo.parameter);
+            }
+        }
+
+        if (meshDataThreadInfoQueue.Count > 0) {
+            for (int i = 0; i < meshDataThreadInfoQueue.Count; i++) {
+                MapThreadInfo<MeshData> threadInfo = meshDataThreadInfoQueue.Dequeue ();
+                threadInfo.callback (threadInfo.parameter);
+            }
+        }
+    }
+
+    MapData GenerateMapData(Vector2 centre) {
+        float[,] noiseMap = Noise.GenerateNoiseMap (mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, centre + offset);
+
         Color[] colorMap = new Color[mapChunkSize * mapChunkSize];
-
-        //Loop through the noise map to get assign the color values.
-        for (int y = 0; y < mapChunkSize; y++)
-        {
-            for (int x = 0; x < mapChunkSize; x++)
-            {
-                float currentHeight = noiseMap[x,y];  // The current height at this coordinate in the noiseMap.
-
-                //Now we'll loop through the current regions to see which region this falls within.
-                for (int i = 0; i < regions.Length; i++)
-                {
-                    if (currentHeight <= regions[i].height)
-                    {
-                        //Before we break we want to assign the color for this place.
-                        colorMap[y * mapChunkSize + x] = regions[i].color;
-
-                        // This means we've found the region that it falls within so we don't need to check the region we can
-                        // move onto the next coordinate.
+        for (int y = 0; y < mapChunkSize; y++) {
+            for (int x = 0; x < mapChunkSize; x++) {
+                float currentHeight = noiseMap [x, y];
+                for (int i = 0; i < regions.Length; i++) {
+                    if (currentHeight <= regions [i].height) {
+                        colorMap [y * mapChunkSize + x] = regions [i].color;
                         break;
                     }
-
-
-
                 }
             }
         }
 
-        MapDisplay display = FindObjectOfType<MapDisplay>();
-        if (drawMode == DrawMode.NoiseMap)
-        {
-            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
-        }
-        else if (drawMode == DrawMode.ColorMap)
-        {
-            display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapChunkSize, mapChunkSize));
-        }
-        else if (drawMode == DrawMode.Mesh)
-        {
-            display.DrawMesh(MeshGenerator.GenerateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail), TextureGenerator.TextureFromColorMap(colorMap, mapChunkSize, mapChunkSize));
-        }
+
+        return new MapData (noiseMap, colorMap);
     }
 
-    // By default, the OnValidate command is called anytime something is changed in the Editor.
-    void OnValidate()
-    {
-        if (lacunarity < 1)
-        {
+    void OnValidate() {
+        if (lacunarity < 1) {
             lacunarity = 1;
         }
-        if (octaves < 0)
-        {
+        if (octaves < 0) {
             octaves = 0;
         }
     }
+
+    struct MapThreadInfo<T> {
+        public readonly Action<T> callback;
+        public readonly T parameter;
+
+        public MapThreadInfo (Action<T> callback, T parameter)
+        {
+            this.callback = callback;
+            this.parameter = parameter;
+        }
+
+    }
+
 }
 
 [System.Serializable]
-public struct TerrainType
-{
+public struct TerrainType {
     public string name;
     public float height;
     public Color color;
+}
+
+public struct MapData {
+    public readonly float[,] heightMap;
+    public readonly Color[] colorMap;
+
+    public MapData (float[,] heightMap, Color[] colorMap)
+    {
+        this.heightMap = heightMap;
+        this.colorMap = colorMap;
+    }
 }
--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -4,44 +4,35 @@ using UnityEngine;
 
 public static class MeshGenerator {
 
-    // For multithreading we will return meshData.  That way when the mesh is generating the game doesn't freeze up.  We'll generate the mesh
-    // data inside the thread and then outside the thread we can "get the new mesh".
+    public static MeshData GenerateTerrainMesh (float[,] heightMap, float heightMultiplier, AnimationCurve _heightCurve, int levelOfDetail) {
+        AnimationCurve heightCurve = new AnimationCurve(_heightCurve.keys);
 
-    public static MeshData GenerateTerrainMesh (float[,] heightMap, float heightMultiplier, AnimationCurve heightCurve, int levelOfDetail)
-    {
         int width = heightMap.GetLength(0);
         int height = heightMap.GetLength(1);
 
-        // We are creating below so we can center our map info.  -1 to 0 to 1 with zero being in the middle.
         float topLeftX = (width - 1) / -2f;
         float TopLeftZ = (height - 1) / 2f;
 
-        int meshSimplificationIncrement = (levelOfDetail == 0)?1:levelOfDetail * 2; //This translates to: if(levelOfDetail == 0), then (?) set it to 1, or else (:) set it to that.
+        int meshSimplificationIncrement = (levelOfDetail == 0)?1:levelOfDetail * 2;
         int verticesPerLine = (width-1)/meshSimplificationIncrement+1;
 
         MeshData meshData = new MeshData(verticesPerLine, verticesPerLine);
         int vertexIndex = 0;
 
-        for (int y = 0; y < height; y+= meshSimplificationIncrement)
-        {
-            for (int x = 0; x < width; x+= meshSimplificationIncrement)
-            {
+        for (int y = 0; y < height; y+= meshSimplificationIncrement) {
+            for (int x = 0; x < width; x+= meshSimplificationIncrement) {
                 meshData.vertices[vertexIndex] = new Vector3(topLeftX + x, heightCurve.Evaluate(heightMap [x,y]) * heightMultiplier, TopLeftZ - y);
                 meshData.uvs[vertexIndex] = new Vector2(x/(float)width, y/(float)height);
 
-                if (x < width -1 && y < height - 1)
-                {
+                if (x < width -1 && y < height - 1){
                     meshData.AddTriangle(vertexIndex, vertexIndex + verticesPerLine + 1, vertexIndex + verticesPerLine); // The first triangle
                     meshData.AddTriangle(vertexIndex + verticesPerLine + 1, vertexIndex, vertexIndex + 1); // The second triangle
                 }
-
                 vertexIndex++;
             }
         }
-
         return meshData;
     }
-
 }
 
 public class MeshData {
@@ -52,23 +43,20 @@ public class MeshData {
 
     int triangleIndex;
 
-    public MeshData(int meshWidth, int meshHeight)
-    {
+    public MeshData(int meshWidth, int meshHeight) {
         vertices = new Vector3[meshWidth * meshHeight];
         uvs = new Vector2[meshWidth * meshHeight];
-        triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 6];  // Multiply by 6 as there are 6 points per quad.
+        triangles = new int[(meshWidth - 1) * (meshHeight - 1) * 6];
     }
 
-    public void AddTriangle (int a, int b, int c)
-    {
+    public void AddTriangle (int a, int b, int c) {
         triangles[triangleIndex] = a;
         triangles[triangleIndex + 1] = b;
         triangles[triangleIndex + 2] = c;
         triangleIndex += 3;
     }
 
-    public Mesh CreateMesh()
-    {
+    public Mesh CreateMesh() {
         Mesh mesh = new Mesh();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
